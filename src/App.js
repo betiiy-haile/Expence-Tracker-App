7e800c0f08e542c0695734bf77f0dce3bfe39e82
betiiy-haile <betelhemhaile74@gmail.com>
2023-04-17 22:47:29 +0300

    Adding ExpenceItem Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+
+  const expences = [
+    {
+      id: "E1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: "E2",
+      title: "New Tv",
+      amount: 799.49,
+      date: new Date(2021, 8, 20)
+    },
+    {
+      id: "E3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: "E4",
+      title: "New Desk(wooden)",
+      amount: 450,
+      date: new Date(2021, 2, 28)
+    }
+  ]
+
     return (
       <div>
         <h2>Let's get started!</h2>
+        <ExpenseItem
+          title={expences[0].title}
+          date={expences[0].date}
+          amount={expences[0].amount}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expences[1].title}
+          date={expences[1].date}
+          amount={expences[1].amount}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expences[2].title}
+          date={expences[2].date}
+          amount={expences[2].amount}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expences[3].title}
+          date={expences[3].date}
+          amount={expences[3].amount}
+        ></ExpenseItem>
       </div>
     );
   }
   
   export default App;
-  
